2e5b0ef4a57af09c01e8c081caa80e94552fc6dc
Dinesh J Subbarao <dineshj888@gmail.com>
2022-01-25 13:55:04 +0530

    form

---
+++ ssp/form.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <title>Wiztoonz form</title>
+    </head>
+    <body>
+        <h3 align=center>Wiztoonz</h3>
+            <form action="#" method="post">
+                <p>UserName: <input type="text" name="username" size="20"maxlength="5"/></p>
+				<p>Password: <input type="password" name="password" size="12"/> </p>
+				<p> <input type="submit" name="submit" value="login"/> </p>
+				<p> <input type="reset" name="reset" value="clearForm"/> </p>
+				
+
+
+            </form>    
+    
+    
+    
+    </body>
+</html>
